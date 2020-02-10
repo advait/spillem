a007e2b6dec3b78527d077a48bfa25b4bb341537
Advait Shinde <advait.shinde@gmail.com>
2020-02-10 22:16:05 +0300

    Create new SessionTest:
      - Parses a spillem program and supports asserting of SpExpressions.

---
+++ tests/SessionArithmeticTest.elm
@@ -0,0 +1,19 @@
+module SessionArithmeticTest exposing (..)
+
+import SessionTestHelper
+import Test exposing (Test, describe)
+
+
+spillem =
+    """
+;; Testing REPL_ENV
+(+ 3 2)
+;=>5
+(/ (- (+ 5 (* 2 3)) 3) 4)
+;=>2
+""" |> String.trim
+
+
+suite : Test
+suite =
+    describe "Basic Arithmetic" <| SessionTestHelper.parseSession spillem
---
+++ tests/SessionTestHelper.elm
@@ -0,0 +1,103 @@
+module SessionTestHelper exposing (parseSession)
+
+import Eval
+import Expect exposing (Expectation)
+import Parser exposing ((|.), (|=), Parser)
+import SpParser
+import Test exposing (Test, test)
+import Types exposing (..)
+
+
+parseSession : String -> List Test
+parseSession input =
+    let
+        reduceSession : List Expectation -> SpState -> List SessionItem -> List Expectation
+        reduceSession acc state sess =
+            case sess of
+                [] ->
+                    acc |> List.reverse
+
+                (Assertion ass) :: tail ->
+                    let
+                        expectation =
+                            Expect.equal state.result (Ok ass)
+                    in
+                    reduceSession (expectation :: acc) state tail
+
+                (Expr expr) :: tail ->
+                    let
+                        nextState =
+                            Eval.eval state expr
+                    in
+                    case nextState.result of
+                        Err err ->
+                            let
+                                newTest =
+                                    Expect.fail err
+                            in
+                            reduceSession (newTest :: acc) nextState tail
+
+                        Ok _ ->
+                            reduceSession acc nextState tail
+
+                (Description _) :: tail ->
+                    -- TODO(advait): Support parsing descriptions
+                    reduceSession acc state tail
+    in
+    case Parser.run sessionParser input of
+        Err _ ->
+            [ Test.test "Failed to parse" (\_ -> Expect.fail "Failed to parse") ]
+
+        Ok session ->
+            let
+                expectations =
+                    reduceSession [] Eval.initState session
+            in
+            expectations
+                |> List.indexedMap
+                    (\i expectation ->
+                        test (String.fromInt i) (\_ -> expectation)
+                    )
+
+
+type SessionItem
+    = Description String
+    | Assertion SpExpression
+    | Expr SpExpression
+
+
+sessionParser : Parser (List SessionItem)
+sessionParser =
+    Parser.succeed identity
+        |= Parser.sequence
+            { start = ""
+            , separator = ""
+            , end = ""
+            , item = itemParser
+            , spaces = SpParser.whitespaceOrTabs
+            , trailing = Parser.Optional
+            }
+        |. Parser.end
+
+
+itemParser : Parser SessionItem
+itemParser =
+    Parser.oneOf [ assertParser, descParser, exprParser ]
+
+
+assertParser =
+    Parser.succeed Assertion
+        |. Parser.symbol ";=>"
+        |= SpParser.expressionParser
+
+
+descParser =
+    Parser.succeed Description
+        |. Parser.symbol ";; "
+        |= Parser.getChompedString (Parser.chompUntilEndOr "\n")
+
+
+exprParser : Parser SessionItem
+exprParser =
+    Parser.succeed Expr
+        |= SpParser.expressionParser
