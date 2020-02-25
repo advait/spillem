6191a82a9e043b474845d1bc7d01a85543f8b94e
Advait Shinde <advait.shinde@gmail.com>
2020-02-25 11:44:41 -0800

    Implement lt, lte, gt, gte, mod in stdlib.

--- src/Stdlib.elm
+++ src/Stdlib.elm
@@ -7,21 +7,54 @@ import Types exposing (..)
 lib : Dict SpSymbol SpExpression
 lib =
     Dict.fromList
-        [ ( "+", BuiltinFun (num2 (+)) )
-        , ( "-", BuiltinFun (num2 (-)) )
-        , ( "*", BuiltinFun (num2 (*)) )
-        , ( "/", BuiltinFun (num2 (//)) )
+        [ ( "+", BuiltinFun (numNumNum (+)) )
+        , ( "-", BuiltinFun (numNumNum (-)) )
+        , ( "*", BuiltinFun (numNumNum (*)) )
+        , ( "/", BuiltinFun (numNumNum (//)) )
+        , ( "%", BuiltinFun (numNumNum (modBy |> flip)) )
+        , ( ">", BuiltinFun (numNumBool (>)) )
+        , ( ">=", BuiltinFun (numNumBool (>=)) )
+        , ( "<", BuiltinFun (numNumBool (<)) )
+        , ( "<=", BuiltinFun (numNumBool (<=)) )
         ]
 
 
-{-| Convert an elm function that takes in two integer arguments into a BuiltinFun with appropriate argument
+{-| Convert an elm function (Int -> Int -> Int) into a BuiltinFun with appropriate argument
 error handling.
 -}
-num2 : (Int -> Int -> Int) -> (List SpExpression -> SpState -> SpState)
-num2 f args state =
+numNumNum : (Int -> Int -> Int) -> (List SpExpression -> SpState -> SpState)
+numNumNum f args state =
     case args of
         [ SpInt x, SpInt y ] ->
             { state | result = Ok <| SpInt (f x y) }
 
         _ ->
             { state | result = Err "RuntimeError" }
+
+
+{-| Convert an elm function (Int -> Int -> Bool) into a BuiltinFun with appropriate argument
+error handling.
+-}
+numNumBool : (Int -> Int -> Bool) -> (List SpExpression -> SpState -> SpState)
+numNumBool f args state =
+    let
+        boolToExpr b =
+            if b then
+                SpSymbol "true"
+
+            else
+                SpSymbol "false"
+    in
+    case args of
+        [ SpInt x, SpInt y ] ->
+            { state | result = Ok (f x y |> boolToExpr) }
+
+        _ ->
+            { state | result = Err "RuntimeError" }
+
+
+{-| Flips the order of arguments in a function.
+-}
+flip : (a -> b -> c) -> (b -> a -> c)
+flip f b a =
+    f a b
